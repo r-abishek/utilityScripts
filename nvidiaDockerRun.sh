92a6787f9800470fdf28beac53e3fe433049541e
Abishek <abishek.genex@gmail.com>
2020-01-29 16:28:51 -0800

    Add nvidia envt setup scripts

---
+++ nvidiaDockerRun.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --gpus all -it --rm nvcr.io/nvidia/cuda:10.1-base-ubuntu18.04
---
+++ nvidiaRepositoryKeysUpdate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
